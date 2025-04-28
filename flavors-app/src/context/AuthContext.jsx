854c65ffbb3295ec411fbeca5084709a4e059700
coverdale <shishkin.nik.228@gmail.com>
2025-04-28 13:49:35 +0500

    Исправленны баги с авторизацией, есть коннект с localStorage API

--- flavors-app/src/context/AuthContext.jsx
+++ flavors-app/src/context/AuthContext.jsx
@@ -1,12 +1,15 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { apiUrl } from '@global/Variables';
 import axios from 'axios';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // Текущий пользователь
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Состояние авторизации
+  const [user, setUser] = useState(() => {
+    const userDataFromLocalStorage = localStorage.getItem("user_data")
+    return JSON.parse(userDataFromLocalStorage)
+  }); // Текущий пользователь
+  const [isAuthenticated, setIsAuthenticated] = useState(Boolean(localStorage.getItem("user_data"))); // Состояние авторизации
   
   // Вход пользователя
   const login = async (username, password) => {
@@ -24,9 +27,10 @@ export const AuthProvider = ({ children }) => {
         username,
         ...userData
       };
-      // Запись пользователя в контекст
+      // Запись пользователя в контекст, localStorage и свитч авторизации с false на true
       setUser(user);
       setIsAuthenticated(true);
+      writeInLocalStorage(user);
 
       return Promise.resolve(user);
     } catch (error) {
@@ -37,8 +41,11 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     setUser(null);
     setIsAuthenticated(false);
+    localStorage.removeItem("user_data")
   };
-
+  // Записать данные в localStorage
+  const writeInLocalStorage = (user) => localStorage.setItem("user_data", JSON.stringify(user))
+  
   return (
     <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
       {children}
--- flavors-app/src/services/FetchAPI.jsx
+++ flavors-app/src/services/FetchAPI.jsx
@@ -1,37 +1,8 @@
 import { apiUrl } from "@global/Variables";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 
-export async function getDataUser() {
-  const accessToken = localStorage.getItem("access_token")
-  if (!accessToken) {
-    throw new Error('Access token not found');
-  }
 
-  try {
-    const response = await fetch(`${apiUrl}/chat/user/me/`, {
-      method: "GET",
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`, // Добавляем токен в заголовок
-      },
-    });
-
-    if (!response.ok) {
-      if (response.status === 401) {
-        throw new Error('Unauthorized');
-      }
-      throw new Error(`Ошибка получения данных с сервера! ${response.status}`);
-    }
-    const data = await response.json();
-    return data
-
-  } catch (error) {
-    console.error('Ошибка при получении:', error);
-    throw error;
-  }
-}
 export async function getAllMessageChat(sender_id, receiver_id) {
   const accessToken = localStorage.getItem("access_token")
   try {
